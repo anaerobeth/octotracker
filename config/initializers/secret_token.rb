91d51a6c0d828bf3e284d2936d66e1b415b3d0c4
Beth Tenorio <betsky98@gmail.com>
2013-09-25 22:00:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Octotracker::Application.config.secret_key_base = '02e47079980bd042b3965fff97de895a59f30a3fbde7494b590e546fb2f5bdca68edc346d89f7e4addfcc76a0da1a6c92333298cf37332121f0c30dad3d7daec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Octotracker::Application.config.session_store :cookie_store, key: '_octotracker_session'
